2471e24c16b4665ea3e76c9f8b049f585d47d888
happywithxpl <15162176893@163.com>
2022-07-13 23:28:38 +0800

    add setup.py 20220713

---
+++ setup.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time    : 2022/7/13 22:58
+# @Author  : xiepulin
+# @File    : setup.py
+# @Software: PyCharm
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time    : 2022/7/6 9:58
+# @Author  : xiepulin
+# @File    : setup_zz.py
+# @Software: PyCharm
+
+"""Setup the GeneMiner environment."""
+
+# Test pip
+# 1) Clean the /dist directory
+# 2) python3 setup.py sdist bdist_wheel
+# 3) pip install --index-url https://test.pypi.org/simple/
+#    --extra-index-url https://pypi.org/simple atram
+# 4) twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+
+
+import platform
+import sys
+import setuptools
+from setuptools import setup
+
+# python libs
+install_dependencies = []
+try:
+    import Bio
+    print(Bio.__version__)
+except ImportError:
+    install_dependencies.append("biopython>=1.70")
+else:
+    sys.stdout.write("Existed module numpy " + str(Bio.__version__) + "\n")
+
+
+
+sys.stdout.write("Python " + str(sys.version).replace("\n", " ") + "\n")
+sys.stdout.write("PLATFORM: " + " ".join(platform.uname()) + "\n")
+sys.stdout.write("Using setuptools " + str(setuptools.__version__) + "\n")
+
+scripts_to_install = ["easy353.py", "build_database.py"]
+
+setup(
+    name="GeneMiner",
+    version="1.0.0",
+    author="xiepulin",
+    author_email="xiepulin@stu.scu.edu.cn , happywithxpl@126.com",
+    description="""GeneMiner : a software for extracting phylogenetic markers from next generation sequencing data""",
+    license="GPL-3.0 license",
+    # 项目主页
+    url="https://github.com/happywithxpl/Geneminer",
+    python_requires='>=3.7',
+    install_requires=install_dependencies,
+    # scripts=scripts_to_install,
+    packages=["lib"],
+    zip_safe=False
+)
